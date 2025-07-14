b9d3e7a37ac9ecc16eec1ff579e062332a44aa0f
Tom E Flaherty <teflaherty@utexas.edu>
2025-07-13 20:05:57 -0500

    Convert Spec level

---
+++ Convert_Spec_Level/cmdConvertSpecLevel.cs
@@ -0,0 +1,38 @@
+﻿using SandBox.Classes;
+using SandBox.Common;
+
+namespace SandBox
+{
+    [Transaction(TransactionMode.Manual)]
+    public class cmdConvertSpecLevel : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            // Revit application and document variables
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document curDoc = uidoc.Document;
+
+            // Your code goes here
+
+
+            return Result.Succeeded;
+        }
+        internal static PushButtonData GetButtonData()
+        {
+            // use this method to define the properties for this command in the Revit ribbon
+            string buttonInternalName = "btnCommand2";
+            string buttonTitle = "Button 2";
+
+            clsButtonData myButtonData = new clsButtonData(
+                buttonInternalName,
+                buttonTitle,
+                MethodBase.GetCurrentMethod().DeclaringType?.FullName,
+                Properties.Resources.Blue_32,
+                Properties.Resources.Blue_16,
+                "This is a tooltip for Button 2");
+
+            return myButtonData.Data;
+        }
+    }
+}
