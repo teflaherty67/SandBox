a1055078a134698ef107f438466dc0f27007f3d8
Tom E Flaherty <teflaherty@utexas.edu>
2025-07-25 14:14:53 -0500

    family updates added

---
+++ Family_Updates/cmdFamilyUpdate.cs
@@ -0,0 +1,49 @@
+﻿using SandBox.Classes;
+
+namespace SandBox
+{
+    [Transaction(TransactionMode.Manual)]
+    public class cmdFamilyUpdate : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            // Revit application and document variables
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            // find & group duplicate family names
+
+            // for each group
+            // target family = base familiy (no numbers)
+            // source families = numbered variants
+
+            // for each source family instance
+            // chnage to corresponding type in target family
+            // if target family does not exist, handle error condition
+
+            // purge source families
+
+            // include progress bar for tracking
+
+            return Result.Succeeded;
+        }
+        internal static PushButtonData GetButtonData()
+        {
+            // use this method to define the properties for this command in the Revit ribbon
+            string buttonInternalName = "btnCommand2";
+            string buttonTitle = "Button 2";
+
+            clsButtonData myButtonData = new clsButtonData(
+                buttonInternalName,
+                buttonTitle,
+                MethodBase.GetCurrentMethod().DeclaringType?.FullName,
+                Properties.Resources.Blue_32,
+                Properties.Resources.Blue_16,
+                "This is a tooltip for Button 2");
+
+            return myButtonData.Data;
+        }
+    }
+
+}
