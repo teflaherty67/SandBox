06532cfb69de301f0b887baa2a98e8d1525e8e1d
ThomasEFlaherty <ThomasEFlaherty@LUSAD_LAPTOP>
2025-09-21 11:29:29 -0500

    rename in progress

---
+++ Schedule_Organization/cmdSchedRename.cs
@@ -0,0 +1,99 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using SandBox.Classes;
+using SandBox.Common;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace SandBox
+{
+    [Transaction(TransactionMode.Manual)]
+    public class cmdSchedRename : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            // Revit application and document variables
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document curDoc = uidoc.Document;
+
+            // list to hold schedules to rename
+            List<ViewSchedule> schedsToRename = new List<ViewSchedule>();
+
+            // variable for regex pattern to match old naming convention
+            string oldPattern = @"[A-Za-z]-\d{2}/[A-Za-z]/[A-Za-z]/([A-Za-z]/)?\w";
+
+            // get all the schedules in the project
+            List<ViewSchedule> allCurSchedules = Utils.GetAllSchedules(curDoc);
+
+            // loop through each schedule
+            foreach (ViewSchedule curSched in allCurSchedules)
+            {
+                // check for old naming convention
+                string schedName = curSched.Name;
+
+                if (Regex.IsMatch(schedName, oldPattern))
+                {
+                    // add it to a list to rename
+                    schedsToRename.Add(curSched);
+                }
+            }            
+
+            // create a transaction to rename the schedules
+            using (Transaction t1 = new Transaction(curDoc, "Rename Schedules"))
+            {
+                // start the transaction
+                t1.Start();
+
+                // loop through the list and rename
+                foreach (ViewSchedule curSched in schedsToRename)
+                {
+                    // get the exisitng name
+                    string curName = curSched.Name;
+
+                    // extract elevation designation from current name using regex
+                    Match elevMatch = Regex.Match(curName, oldPattern);
+                    if (elevMatch.Success)
+                    {
+                        // get first character of the old pattern
+                        string elevLetter = elevMatch.Value.Substring(0, 1); // Get first char of the matched pattern
+
+                        // create the new pattern
+                        string newPattern = "Elevation " + elevLetter;
+
+                        // replace old pattern with new pattern
+                        string newName = curName.Replace(elevMatch.Value, newPattern);
+
+                        // rename the schedule
+                        curSched.Name = newName;
+                    }
+                }
+
+                // commit the transaction
+                t1.Commit();
+            }
+
+            // notify the user of completion
+            Utils.TaskDialogInformation("Success", "Rename Schedules", $"Renamed {schedsToRename.Count} schedules.");
+
+            return Result.Succeeded;
+        }
+
+        internal static PushButtonData GetButtonData()
+        {
+            // use this method to define the properties for this command in the Revit ribbon
+            string buttonInternalName = "btnCommand2";
+            string buttonTitle = "Button 2";
+
+            clsButtonData myButtonData = new clsButtonData(
+                buttonInternalName,
+                buttonTitle,
+                MethodBase.GetCurrentMethod().DeclaringType?.FullName,
+                Properties.Resources.Blue_32,
+                Properties.Resources.Blue_16,
+                "This is a tooltip for Button 2");
+
+            return myButtonData.Data;
+        }
+    }
+}
